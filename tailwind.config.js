010045632398a0ff4b6d4880abf2e35c6910e847
coder-boy-Laju <mdlaju43@gmail.com>
2024-08-09 20:47:36 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "manrope":["Manrope", ...defaultTheme.fontFamily.sans]
+      },
+      colors:{
+        transparent: 'transparent',
+        current: 'currentColor',
+       "primary":"#FF0000",
+        "secondary":"#FF8938",
+        "gray-1": "#1C1C1C",
+        "gray-2": "#777777",
+       "gray-3": "#494949",
+        "gray-4": "#D2D2D2",
+        "white": "#FFFFFF",
+        "white-light":" #F4F4F4",
+    
+      },
+    },
+  },
+  plugins: [],
+}
+
